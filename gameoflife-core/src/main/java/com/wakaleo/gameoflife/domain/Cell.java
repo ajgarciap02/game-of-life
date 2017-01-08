c98b58807a8d58bd881fc64e0bb1adfc2fc177f1
alvaro garcia <alvarogarp@gmail.com>
2017-01-08 20:40:49 +0100

    --

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,13 +1,12 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    LIVE_CELL("c"), DEAD_CELL(".");
+    LIVE_CELL("-"), DEAD_CELL(".");
 
     private String symbol;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
-        int pepe="a";
     }
 
     @Override
